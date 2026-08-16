4f8d09249f22927a0815c3b8d1ec26aaf4a6a009
pablomtarga <pablomtarga@outlook.com>
2026-08-16 15:45:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='iron-v9';
+const CACHE='iron-v10';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));
   self.skipWaiting();
